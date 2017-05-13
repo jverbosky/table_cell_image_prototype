5815608526eb977960e76bba1338a80a380811db
jverbosk <jverbosk@gmail.com>
2017-05-13 14:39:40 -0400

    image successfully resizing to dynamic table cells (NxN board)

---
+++ app.rb
@@ -0,0 +1,14 @@
+require 'sinatra'
+
+get '/' do
+  # rows = [["", "", "X"], ["O", "O", "X"], ["X", "", ""]]
+  # rows = [["", "", "O", "X"], ["O", "", "X", ""], ["", "X", "", "O"], ["X", "", "O", ""]]
+  rows = [["O", "", "O", "X", ""], ["O", "", "X", "", "O"], ["", "", "X", "", "O"], ["X", "", "O", "O", ""], ["", "O", "X", "", "O"]]
+  # rows = [["", "O", "", "O", "X", ""], ["O", "", "", "X", "", "O"], ["X", "", "", "X", "", "O"], ["X", "", "O", "", "O", ""], ["O", "", "O", "X", "", "O"], ["", "X", "", "", "X", "O"]]
+  erb :table, locals: {rows: rows}
+end
+
+post '/board_index' do
+  board_index = params[:board_index]
+  "Selected board index: #{board_index}"
+end
